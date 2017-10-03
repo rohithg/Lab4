4fb0753eb339de0641cebf7b61a25cc662354778
rohithgangapuram <rohithgangapuram@wirelessprv-10-192-166-145.near.illinois.edu>
2017-10-02 21:37:34 -0500

    Lab done

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -8,19 +8,47 @@
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+/**
+ * Main class.
+ */
+public class ReclamationProject {
+
+    /**
+     * @param args main
+     */
+    public static void main(final String[] args) {
+        System.out.println(doit("Horero", "Wdored"));
+    }
+    /**
+     * @param a is string 1
+     * @param b is string 2
+     * @return The longest matching substring between a and b
+     */
+    static String doit(final String a, final String b) {
+        String result = "";
+        String filteredInput1 = a;
+        String filteredInput2 = b;
+        if (a.length() < a.length()) {
+            String temp = a;
+            filteredInput1 = b;
+            filteredInput2 = temp;
+        }
+
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < filteredInput1.length(); i++) {
+            for (int j = filteredInput1.length() - i; j > 0; j--) {
+                for (int k = 0; k < filteredInput2.length() - j; k++) {
+                    if (filteredInput1.regionMatches(i, filteredInput2, k, j)
+                            && j > result.length()) {
+                        result = filteredInput1.substring(i, i + j);
+                    }
+                    // Do it!
+
+                }
+            } // Ah yeah
+        }
+        return result;
+    }
 }
